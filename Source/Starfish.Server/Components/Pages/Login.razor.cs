6fb8f8baab022aab42fbac7c0f7fa3a2d4c20a11
damon <zhaorong@outlook.com>
2026-01-10 13:36:51 +0800

    Update favicon.png with new version

--- Source/Starfish.Server/Components/Pages/Login.razor.cs
+++ Source/Starfish.Server/Components/Pages/Login.razor.cs
@@ -2,7 +2,9 @@ using System.Net;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Build.Framework;
 using Nerosoft.Starfish.Facade.Transit;
 
 namespace Nerosoft.Starfish.Server.Components.Pages;
@@ -12,16 +14,41 @@ public partial class Login : ComponentBase
 	[CascadingParameter]
 	private HttpContext? HttpContext { get; set; }
 
+	private EditContext editContext = default!;
+
 	private string ReturnUrl => HttpContext?.Request.Query["returnUrl"] ?? "/";
 
-	private string Username { get; set; } = string.Empty;
+	[SupplyParameterFromForm]
+	private LoginModel Model { get; set; } = default;
 
-	private string Password { get; set; } = string.Empty;
+	private bool Loading { get; set; } = false;
+
+	protected override async Task OnInitializedAsync()
+	{
+		Model ??= new LoginModel();
+		editContext = new EditContext(Model);
+	}
 
 	private async Task OnLoginAsync(MouseEventArgs args)
 	{
-		var result = await Service.GrantAsync("Cookies", new AuthRequestDto { Username = Username, Password = Password, GrantType = "username" });
+		var result = await Service.GrantAsync("Cookies", new AuthRequestDto { Username = Model.Username, Password = Model.Password, GrantType = "username" });
 		await HttpContext.SignInAsync("Cookies", new ClaimsPrincipal(new ClaimsIdentity(new Claim[] { })));
 		HttpContext.Response.Redirect(ReturnUrl);
+	}
+
+	private async Task OnSubmitAsync(EditContext context)
+	{
+		var result = await Service.GrantAsync("Cookies", new AuthRequestDto { Username = Model.Username, Password = Model.Password, GrantType = "username" });
+		await HttpContext.SignInAsync("Cookies", new ClaimsPrincipal(new ClaimsIdentity(new Claim[] { })));
+		HttpContext.Response.Redirect(ReturnUrl);
+	}
+
+	private class LoginModel
+	{
+		[Required]
+		public string Username { get; set; } = string.Empty;
+
+		[Required]
+		public string Password { get; set; } = string.Empty;
 	}
 }
