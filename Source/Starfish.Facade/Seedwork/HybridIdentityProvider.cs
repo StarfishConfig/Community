b091067ed51e919f602a5ead8d2ec341eda3f07d
damon <zhaorong@outlook.com>
2026-01-14 13:23:28 +0800

    Add HybridIdentityProvider implementation for identity management

---
+++ Source/Starfish.Facade/Seedwork/HybridIdentityProvider.cs
@@ -0,0 +1,20 @@
+using System.Security.Principal;
+using Nerosoft.Euonia.Bus;
+using Nerosoft.Euonia.Modularity;
+
+namespace Nerosoft.Starfish.Facade;
+
+internal class HybridIdentityProvider : IIdentityProvider
+{
+	private readonly IRequestContextAccessor _accessor;
+
+	public HybridIdentityProvider(IRequestContextAccessor accessor)
+	{
+		_accessor = accessor;
+	}
+
+	public IPrincipal GetIdentity(string authorization)
+	{
+		return _accessor.Context.User;
+	}
+}
