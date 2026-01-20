b619d572dee76cc97fdfc572ee4bdd3091e1dc27
damon <zhaorong@outlook.com>
2026-01-20 10:56:23 +0800

    Split user edit into create/update pages; improve validation

--- Source/Starfish.Domain/Identity/Handlers/UserCommandHandler.cs
+++ Source/Starfish.Domain/Identity/Handlers/UserCommandHandler.cs
@@ -16,75 +16,78 @@ internal sealed class UserCommandHandler(IServiceProvider provider)
 	public Task HandleAsync(UserCreateCommand message, MessageContext context, CancellationToken cancellationToken = default)
 	{
 		return Actuator.For<User>()
-		               .Create(message.Username, message.Password, cancellationToken)
-		               .Handle(user =>
-		               {
-			               user.SetPassword(message.Password, UserPasswordUpdateType.Create);
-			               user.SetEmail(message.Email);
-			               user.SetPhone(message.Phone);
-			               user.SetNickname(message.Nickname);
-			               user.AssignRoles(message.Roles.ToArray());
-			               user.MarkAsNew();
-		               })
-		               .ExecuteAsync(cancellationToken);
+					   .Create(message.Username, cancellationToken)
+					   .Handle(user =>
+					   {
+						   user.SetPassword(message.Password, UserPasswordUpdateType.Create);
+						   user.SetEmail(message.Email);
+						   user.SetPhone(message.Phone);
+						   user.SetNickname(message.Nickname);
+						   if (message.Roles != null)
+						   {
+							   user.AssignRoles(message.Roles.ToArray());
+						   }
+						   user.MarkAsNew();
+					   })
+					   .ExecuteAsync(cancellationToken);
 	}
 
 	public Task HandleAsync(UserUpdateCommand message, MessageContext context, CancellationToken cancellationToken = default)
 	{
 		return Actuator.For<User>()
-		               .Fetch(message.Id, cancellationToken)
-		               .Handle(user =>
-		               {
-			               user.SetEmail(message.Email);
-			               user.SetPhone(message.Phone);
-			               user.SetNickname(message.Nickname);
-			               user.MarkAsChanged();
-		               })
-		               .ExecuteAsync(cancellationToken);
+					   .Fetch(message.Id, cancellationToken)
+					   .Handle(user =>
+					   {
+						   user.SetEmail(message.Email);
+						   user.SetPhone(message.Phone);
+						   user.SetNickname(message.Nickname);
+						   user.MarkAsChanged();
+					   })
+					   .ExecuteAsync(cancellationToken);
 	}
 
 	public Task HandleAsync(UserPasswordChangeCommand message, MessageContext context, CancellationToken cancellationToken = default)
 	{
 		return Actuator.For<User>()
-		               .Fetch(message.Id, cancellationToken)
-		               .Handle(user =>
-		               {
-			               user.SetPassword(message.Password, UserPasswordUpdateType.Change);
-			               user.MarkAsChanged();
-		               })
-		               .ExecuteAsync(cancellationToken);
+					   .Fetch(message.Id, cancellationToken)
+					   .Handle(user =>
+					   {
+						   user.SetPassword(message.Password, UserPasswordUpdateType.Change);
+						   user.MarkAsChanged();
+					   })
+					   .ExecuteAsync(cancellationToken);
 	}
 
 	public Task HandleAsync(UserPasswordResetCommand message, MessageContext context, CancellationToken cancellationToken = default)
 	{
 		return Actuator.For<User>()
-		               .Fetch(message.Id, cancellationToken)
-		               .Handle(user =>
-		               {
-			               user.SetPassword(message.Password, UserPasswordUpdateType.Reset);
-			               user.MarkAsChanged();
-		               })
-		               .ExecuteAsync(cancellationToken);
+					   .Fetch(message.Id, cancellationToken)
+					   .Handle(user =>
+					   {
+						   user.SetPassword(message.Password, UserPasswordUpdateType.Reset);
+						   user.MarkAsChanged();
+					   })
+					   .ExecuteAsync(cancellationToken);
 	}
 
 	public Task HandleAsync(UserFailedCountChangeCommand message, MessageContext context, CancellationToken cancellationToken = new CancellationToken())
 	{
 		return Actuator.For<User>()
-		               .Fetch(message.Id, cancellationToken)
-		               .Handle(user =>
-		               {
-			               switch (message.ActionType)
-			               {
-				               case "+":
-					               user.IncreaseAccessFailedCount();
-					               break;
-				               case "-":
-					               user.ResetAccessFailedCount();
-					               break;
-			               }
+					   .Fetch(message.Id, cancellationToken)
+					   .Handle(user =>
+					   {
+						   switch (message.ActionType)
+						   {
+							   case "+":
+								   user.IncreaseAccessFailedCount();
+								   break;
+							   case "-":
+								   user.ResetAccessFailedCount();
+								   break;
+						   }
 
-			               user.MarkAsChanged();
-		               })
-		               .ExecuteAsync(cancellationToken);
+						   user.MarkAsChanged();
+					   })
+					   .ExecuteAsync(cancellationToken);
 	}
 }
--- Source/Starfish.Shared/Constants/RegexPattern.cs
+++ Source/Starfish.Shared/Constants/RegexPattern.cs
@@ -13,4 +13,21 @@ public static class RegexPattern
 	/// - A valid email address
 	/// </summary>
 	public const string Username = @"^(?:(?=.{3,20}$)[a-zA-Z0-9](?:[a-zA-Z0-9_.]*[a-zA-Z0-9_])?|1[3-9]\d{9}|[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$";
+
+	/// <summary>
+	/// Regex pattern for validating phone numbers, which can be:
+	/// - Optional leading plus sign
+	/// - Optional country code of 1 to 3 digits
+	/// - Optional separators such as spaces, dots, or hyphens
+	/// - Optional area code enclosed in parentheses
+	/// - Three digits followed by a separator and four digits
+	/// </summary>
+	public const string PhoneNumber = @"^[+]?(\d{1,3})?[\s.-]?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$";
+
+	/// <summary>
+	/// Regular expression for matching an email address.
+	/// </summary>
+	/// <remarks>General Email Regex (RFC 5322 Official Standard) from https://emailregex.com.</remarks>
+	public const string EmailAddress = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+
 }
