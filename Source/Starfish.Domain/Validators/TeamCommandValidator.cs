3e13172e4a1a904bff6e7e3ab104b87e0e67a73a
damon <zhaorong@outlook.com>
2026-01-22 11:27:58 +0800

    Add validators for TeamCreateCommand and TeamUpdateCommand to enforce validation rules

---
+++ Source/Starfish.Domain/Validators/TeamCommandValidator.cs
@@ -0,0 +1,31 @@
+using FluentValidation;
+using Nerosoft.Starfish.Domain.Commands;
+using Nerosoft.Starfish.Shared;
+
+namespace Nerosoft.Starfish.Domain.Validators;
+
+internal class TeamCreateCommandValidator : AbstractValidator<TeamCreateCommand>
+{
+	public TeamCreateCommandValidator()
+	{
+		RuleFor(x => x.Name)
+			.NotEmpty().WithMessage("Team name is required.")
+			.MaximumLength(TeamConstants.NameMaximumLength).WithMessage("Team name cannot exceed 100 characters.");
+
+		RuleFor(x => x.Description)
+			.MaximumLength(TeamConstants.DescriptionMaximumLength).WithMessage("Team description cannot exceed 500 characters.");
+	}
+}
+
+internal class TeamUpdateCommandValidator : AbstractValidator<TeamUpdateCommand>
+{
+	public TeamUpdateCommandValidator()
+	{
+		RuleFor(x => x.Name)
+			.NotEmpty().WithMessage("Team name is required.")
+			.MaximumLength(TeamConstants.NameMaximumLength).WithMessage("Team name cannot exceed 100 characters.");
+
+		RuleFor(x => x.Description)
+			.MaximumLength(TeamConstants.DescriptionMaximumLength).WithMessage("Team description cannot exceed 500 characters.");
+	}
+}
