4d2a6b2f7dedc04bcb58bbbf04b90cd2d0a1fca9
damon <zhaorong@outlook.com>
2026-02-05 00:23:58 +0800

    Implement ConfigurationRepository with SaveAsync and GetAsync methods

--- Source/Starfish.Persistent/Repositories/IConfigurationRepository.cs
+++ Source/Starfish.Persistent/Repositories/IConfigurationRepository.cs
@@ -1,6 +1,12 @@
+using Nerosoft.Starfish.Persistent.Data;
+
 namespace Nerosoft.Starfish.Persistent.Repositories;
 
 internal interface IConfigurationRepository : IRepository
 {
+	Task<long> SaveAsync(ConfigurationData data, CancellationToken cancellationToken = default);
+
+	Task<ConfigurationData> GetAsync(long id, CancellationToken cancellationToken = default);
+
 	Task<bool> CheckCodeExistsAsync(long teamId, string code, long excludeId = 0, CancellationToken cancellationToken = default);
 }
---
+++ Source/Starfish.Repository/Repositories/ConfigurationRepository.cs
@@ -0,0 +1,40 @@
+using Nerosoft.Euonia.Mapping;
+using Nerosoft.Starfish.Persistent.Data;
+using Nerosoft.Starfish.Persistent.Repositories;
+using Nerosoft.Starfish.Repository.Entities;
+
+namespace Nerosoft.Starfish.Repository.Repositories;
+
+internal class ConfigurationRepository(ConfigsDataContext context) : IConfigurationRepository
+{
+	public async Task<long> SaveAsync(ConfigurationData data, CancellationToken cancellationToken = default)
+	{
+		Configuration entity;
+
+		if (data.Id > 0)
+		{
+			entity = await context.Set<Configuration>().FindAsync([data.Id], cancellationToken);
+			TypeAdapter.ProjectedAs(data, entity);
+		}
+		else
+		{
+			entity = TypeAdapter.ProjectedAs<Configuration>(data);
+			await context.Set<Configuration>().AddAsync(entity, cancellationToken);
+		}
+
+		await context.SaveChangesAsync(true, cancellationToken);
+		return entity!.Id;
+	}
+
+	public async Task<ConfigurationData> GetAsync(long id, CancellationToken cancellationToken = default)
+	{
+		var entity = await context.Set<Configuration>().FindAsync([id], cancellationToken);
+
+		return TypeAdapter.ProjectedAs<ConfigurationData>(entity);
+	}
+
+	public Task<bool> CheckCodeExistsAsync(long teamId, string code, long excludeId = 0, CancellationToken cancellationToken = default)
+	{
+		throw new NotImplementedException();
+	}
+}
