1676868b92c3f48bf15819b7c823f5fcc1fb81ba
damon <zhaorong@outlook.com>
2026-02-24 19:23:09 +0800

    Add project command classes and initial handler skeleton

---
+++ Source/Starfish.Domain/Commands/ProjectDeleteCommand.cs
@@ -0,0 +1,23 @@
+﻿using Nerosoft.Euonia.Domain;
+
+namespace Nerosoft.Starfish.Domain.Commands;
+
+/// <summary>
+/// Represents a command to delete an existing project entity.
+/// </summary>
+public class ProjectDeleteCommand : Command
+{
+	/// <summary>
+	/// Initializes a new instance of the ProjectDeleteCommand class with the specified project identifier.
+	/// </summary>
+	/// <param name="id">The unique identifier of the project to be deleted.</param>
+	public ProjectDeleteCommand(long id)
+	{
+		Id = id;
+	}
+
+	/// <summary>
+	/// Gets the unique identifier for the entity.
+	/// </summary>
+	public long Id { get; }
+}
---
+++ Source/Starfish.Domain/Handlers/ProjectCommandHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace Nerosoft.Starfish.Domain.Handlers;
+
+internal sealed class ProjectCommandHandler(IServiceProvider provider)
+	: CommandHandlerBase(provider)
+{
+}
