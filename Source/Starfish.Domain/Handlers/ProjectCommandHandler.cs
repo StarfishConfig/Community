1782ac51a600fc4202177c8826bc5c6ddf3c436b
damon <zhaorong@outlook.com>
2026-02-24 20:18:00 +0800

    Remove Project aggregate, commands, and related resources

--- Source/Starfish.Domain/Handlers/ProjectCommandHandler.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Nerosoft.Starfish.Domain.Handlers;
-
-internal sealed class ProjectCommandHandler(IServiceProvider provider)
-	: CommandHandlerBase(provider)
-{
-}
--- Source/Starfish.Persistent/Repositories/IProjectRepository.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Nerosoft.Starfish.Persistent.Repositories;
-
-internal interface IProjectRepository : IRepository
-{
-	Task<bool> CheckNameDuplicationAsync(long teamId, string name, long excludeId = 0, CancellationToken cancellationToken = default);
-}
